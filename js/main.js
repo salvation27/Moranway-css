bea22e0f648fbad76fc0dcf36d01127b9505f3f4
Nikolay Serdun <nikolayzalipykin@gmail.com>
2021-02-11 02:16:58 +0200

    mobile

--- js/main.js
+++ js/main.js
@@ -1,37 +1,54 @@
 $('.autoplay').slick({
   slidesToShow: 4,
   slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 3000,
-  speed: 1000,
+  // autoplay: true,
+  // autoplaySpeed: 3000,
+  // speed: 1000,
+  responsive: [
+    {
+      breakpoint: 850,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 650,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+     {
+      breakpoint: 435,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
 
 
 $('.responsive').slick({
-  dots: true,
-  infinite: true,
-  speed: 300,
+  dots: false,
   slidesToShow: 3,
   slidesToScroll: 3,
   responsive: [
     {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
+      breakpoint: 1100,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
       }
     },
     {
-      breakpoint: 480,
+      breakpoint: 670,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1
